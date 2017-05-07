0e908a34f4c430e780874507444719156053fa78
Mads Obitsø <madsobitsoe@gmail.com>
2017-05-07 10:42:28 +0200

    trying to avoid global variables

--- snakecager.py
+++ snakecager.py
@@ -10,10 +10,12 @@ m = 3
 n = 3
 matrix = [[rowop.F(1) for i in range(m)] for j in range(n)]
 def updateMatrix(form):
+    newMatrix = list(matrix)
     for key in sorted(form.keys()):
         if (key[0] == "a"):
             val = form[key]
-            matrix[int(key[1])-1][int(key[2])-1] = rowop.F(val)
+            newMatrix[int(key[1])-1][int(key[2])-1] = rowop.F(val)
+    return newMatrix
 
 def toMatrix(form):
     updateMatrix(form)
@@ -30,21 +32,14 @@ def toMatrix(form):
 def index():
     if request.method == "GET":
         return render_template("index.html", comments=comments, matrix=matrix)
-    updateMatrix(request.form)
-    #comments.append(toMatrix(request.form))
-    #for key in sorted(request.form):
-        #comments.append("Key: %s, value: %s" % (key, request.form[key]))
     try:
-        global matrix
-        #comments.append(rowop.fracToLatexMatrix(matrix))
-
-        #comments.append(rowop.fracToLatexMatrix(matrix))
+        newMatrix = updateMatrix(request.form)
         row1 = int(request.form["row1"])
         row2 = int(request.form["row2"])
         mul = request.form["multiplier"]
         op = request.form["op"]
         comments.append("Here is your latex:")
-        comments.append(rowop.printRowOp(matrix, row1, row2, rowop.F(mul), op))
+        comments.append(rowop.printRowOp(newMatrix, row1, row2, rowop.F(mul), op))
     except:
         pass
 
