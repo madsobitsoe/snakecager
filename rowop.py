025b7968b22b1f393bc01b36c97deffa52d37c15
Mads Obitsø <madsobitsoe@gmail.com>
2017-05-07 02:02:35 +0200

    Added fractions for use in matrices
    also with conversion to latex

--- rowop.py
+++ rowop.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+from fractions import Fraction as F
 # Requires this in preamble of latex doc
 #\usepackage[all]{xy}
 #\usepackage{setspace}
 # Only works with integers
 
 matrix = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+# define a matrix using fractions
+fracmatrix = [[F(3,2),F(0,1),F(0,1),F(0,1)],[F(0,1),F(1,1),F(0,1),F(0,1)],[F(0,1),F(0,1),F(1,1),F(0,1)],[F(0,1),F(0,1),F(0,1),F(1,1)]]
 
 def addRow(matrix, row1, row2, multiplier):
     for i in range(len(matrix[row1])-1):
@@ -38,12 +41,25 @@ def toLatexMatrix(matrix):
     s = "\\left[\n\\begin{array}{%s}\n" % (n * "r")
     for row in matrix:
         for col in row:
-            s += " %d &" % col
+            s += " %s &" % col
         s = s[:-1]    
         s += " \\\\\n"
     s = s[:-5]
     s += "\n\\end{array}\n\\right]\n"
     return s
+
+def fracToLatexMatrix(matrix):
+    n = len(matrix[0])
+    s = "\\left[\n\\begin{array}{%s}\n" % (n * "r")
+    for row in matrix:
+        for col in row:
+            s += " %s &" % fracToLatex(col)
+        s = s[:-1]    
+        s += " \\\\\n"
+    s = s[:-5]
+    s += "\n\\end{array}\n\\right]\n"
+    return s
+
 
 def createOpString(stringMultiplier, row1, row2, op):
     s = ""
@@ -62,7 +78,7 @@ def printRowOp(matrix, row1, row2, multiplier, op):
          "\\setlength{\\jot}{8pt}\n")
     s += "  \\begin{array}{lcl}"
     # add first matrix"
-    s += "%s\n&\n" % toLatexMatrix(matrix)
+    s += "%s\n&\n" % fracToLatexMatrix(matrix)
     # Add rowop line
     s +=   ("\\xymatrix@C=15ex{\n"
             "    \\ar[r]^-{\\small\n"
@@ -77,13 +93,20 @@ def printRowOp(matrix, row1, row2, multiplier, op):
         subtractRow(matrix, row1, row2, multiplier)
     elif (op == "swap"):
         swapRows(matrix, row1, row2)
-    s += "\n&\n%s" % toLatexMatrix(matrix)
+    s += "\n&\n%s" % fracToLatexMatrix(matrix)
     s += "\\end{array}"
     print s
-    
+
+def fracToLatex(frac):
+    split = str(frac).split("/")
+    if (len(split) == 2):
+        return "\\frac{%s}{%s}" % (split[0], split[1])
+    return split[0]
+
 # Do some matrix operations
 if __name__ == "__main__":
-    printRowOp(matrix, 1, 3, 0, "swap")
-    printRowOp(matrix, 1, 2, -2, "-")
-    printRowOp(matrix, 1, 3, -2, "+")
-    
+    printRowOp(fracmatrix, 1, 3, 0, "swap")
+    printRowOp(fracmatrix, 1, 3, -7, "-")
+    printRowOp(fracmatrix, 1, 3, -2, "+")
+    printRowOp(fracmatrix, 3, 1, -7, "+")
+
